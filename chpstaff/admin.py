640616044a42c80ba0c82a507756300ea163b4a3
michmaml <mjsekulski1@gmail.com>
2020-04-25 00:27:05 +0800

    Removed redundant Account model

--- chpstaff/admin.py
+++ chpstaff/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from .models import Account
-# Register your models here.
-
-admin.register(Account)
--- trans_19/admin.py
+++ trans_19/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Patient, Case
-from chpstaff.models import Account
+#from chpstaff.models import Account
 # Register your models here.
 
 admin.site.register(Patient)
 admin.site.register(Case)
-admin.site.register(Account)
+# admin.site.register(Account)
