ddcb4b8c6d8abaa0c7efa56003e305c902d8f9f5
Wong Ngai Sum <wongngaisum@protonmail.com>
2020-05-14 23:06:51 +0800

    Switching to PostgreSQL

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trans19',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
